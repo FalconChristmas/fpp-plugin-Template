6e7e819fdd08a2bc13443253cef8abc803c64a85
Kenny Robinson <bitsecondal@gmail.com>
2023-03-26 10:33:08 -0500

    Corrected misspelling; removed unmatched div; added full opening tag

--- about.php
+++ about.php
@@ -1,17 +1,16 @@
-  <div style="margin:0 auto;"> <br />
+<div style="margin:0 auto;"> <br />
     <fieldset style="padding: 10px; border: 2px solid #000;">
-      <legend>Template Plugin Info</legend>
-      <div style="overflow: hidden; padding: 10px;">
-    <div>
-      <div id='credits'>
-        <b>Template Plugin Developed By:</b><br />
-		<br />
-        John Doe (jdoe)<br />
-		<br />
-        <a href='https://github.com/FalconChristmas/fpp-plugin-Template'>Git Repository</a><br>
-        <a href='https://github.com/FalconChristmas/fpp-plugin-Template/issues'>Bug Reporter</a><br>
-		<br />
-      </div>
-    </div>
+        <legend>Template Plugin Info</legend>
+        <div style="overflow: hidden; padding: 10px;">
+            <div id='credits'>
+                <b>Template Plugin Developed By:</b><br />
+                <br />
+                John Doe (jdoe)<br />
+                <br />
+                <a href='https://github.com/FalconChristmas/fpp-plugin-Template'>Git Repository</a><br>
+                <a href='https://github.com/FalconChristmas/fpp-plugin-Template/issues'>Bug Reporter</a><br>
+                <br />
+            </div>
+        </div>
     </fieldset>
-  </div>
+</div>
